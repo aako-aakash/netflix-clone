61c01c4e06027187b340a2215e430bf937eb1d24
aako-aakash <aako27082004@gmail.com>
2026-01-31 15:49:06 +0530

    Initial project setup with React + Tailwind

---
+++ src/app/App.jsx
@@ -0,0 +1,12 @@
+const App = () => {
+  return (
+    <div className="min-h-screen">
+      <h1 className="text-netflix text-3xl font-bold text-center mt-10">
+        Netflix Clone
+      </h1>
+      <h2>AAKASH KUMAR SAW !!!</h2>
+    </div>
+  );
+};
+
+export default App;
